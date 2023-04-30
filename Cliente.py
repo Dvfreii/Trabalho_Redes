55f14bc05355d17f2e441516001bc7ce00e8ff26
Dvfre <davifreire@alu.ufc.br>
2023-04-30 18:27:55 -0300

    Trab_Finalizado

--- Cliente.py
+++ Cliente.py
@@ -1,27 +1,31 @@
-# -*- coding: utf-8 -*-
+''' Nome: Davi Freire Azevedo | Matricula: 498905 '''
 import socket
 import random
 import time
 
 def gerador():
-    gerar_num = random.randint(0, 10**30)
+    gerar_num = str(random.randint(0, 10**30))
     return gerar_num
 
 
 
 def cliente():
-    IP= '127.0.0.1'
-    Port= 5000
-    cliente= socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    cliente.connect((IP, Port))
-
     print('Conectado ao servidor')
     while True:
+        IP= '127.0.0.1'
+        Port= 5000
+        cliente= socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        cliente.connect((IP, Port))
         
         gerar = gerador()
-        cliente.sendall(str.encode(str(gerar)))
-        recebe= cliente.recv(1024)
-        print('Servidor recebeu: ', str(recebe,'utf-8'))
+        print("Número gerado:", gerar)
+
+        cliente.sendall(gerar.encode())
+        recebe= cliente.recv(1024).decode()
+        print('Servidor recebeu: ', recebe)
+
+        cliente.close()
+        print("FIM")
 
         time.sleep(10)
 
--- Servidor.py
+++ Servidor.py
@@ -1,6 +1,25 @@
-# -*- coding: utf-8 -*-
+''' Nome: Davi Freire Azevedo | Matricula: 498905 '''
 import socket
 
+def divisivel(NUM):
+    return NUM % 2 == 0  
+
+
+def verifica(conexao):
+    recebe = conexao.recv(1024).decode() 
+    print('Numero: ',recebe)
+
+    if len(recebe) > 10:
+        resposta = str(recebe)
+    else:
+        if divisivel(int(recebe)):
+            resposta = "PAR"
+        else:
+            resposta = "ÍMPAR"
+    conexao.sendall(resposta.encode())
+    conexao.close()
+
+            
 
 def servidor():
     IP='localhost'
@@ -14,11 +33,8 @@ def servidor():
     while True:
         conexao, cliente = servidor.accept()
         print('Conectado em', cliente)
-        while True:
-            recebe = conexao.recv(1024) 
-           
-            print(cliente, 'disse: ',str(recebe,'utf-8'))
+        verifica(conexao)
+            
 
-            conexao.sendall(str.encode(str(recebe,'utf-8')))
 servidor()
             
